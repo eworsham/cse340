96b6b2b98ecbe33d078067ca120e31d13f090c5d
eworsham <eworsham64@gmail.com>
2024-09-27 19:00:29 -0400

    Removed alter type statement that isn't needed I think (it's causing issues, but works without it)

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -1,6 +1,5 @@
 -- Create the account type enum
 CREATE TYPE public.account_type AS ENUM ('Client', 'Employee', 'Admin');
-ALTER TYPE public.account_type OWNER TO cse340;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
