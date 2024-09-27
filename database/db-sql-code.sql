c078f1007d49b834f43cdc5508097a5f2eae016f
eworsham <eworsham64@gmail.com>
2024-09-27 18:44:23 -0400

    Add some missed public keywords

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -234,7 +234,7 @@ VALUES   (
   );
 
 -- Query 4 - Update GM Hummer description
-UPDATE inventory
+UPDATE public.inventory
 SET inv_description = REPLACE(
         inv_description,
         'the small interiors',
@@ -244,6 +244,6 @@ WHERE inv_make = 'GM'
     AND inv_model = 'Hummer';
 
 -- Query 6 - Update image and thumbnail urls
-UPDATE inventory
+UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, 'images', 'images/vehicles'),
     inv_thumbnail = REPLACE(inv_thumbnail, 'images', 'images/vehicles');
