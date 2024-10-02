91cf24ccc35459d34fa6df45f7d1851782cbc789
eworsham <eworsham64@gmail.com>
2024-10-02 09:37:19 -0400

    added util for getting nav, updated nav ejs file to be dynamic

---
+++ utilities/index.js
@@ -0,0 +1,24 @@
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* *****************************
+ * Constructs the nav HTML unordered list
+ * ***************************** */
+Util.getNav = async function (req, res, next) {
+    let data = await invModel.getClassifications()
+    let list = `
+        <ul>
+            <li><a href="/" title="Home page">Home</a></li>
+    `
+    data.rows.foreach(row => {
+        list += `
+            <li>
+                <a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a>
+            </li>
+        `
+    })
+    list += '</ul>'
+    return list
+}
+
+module.exports = Util
