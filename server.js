63b9befd90dd0a818045ad652f73a3c185e186d4
eworsham <eworsham64@gmail.com>
2024-10-03 09:38:40 -0400

    Improved the error handling

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory route
 app.use("/inv", inventoryRoute)
@@ -47,9 +47,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  err.status === 404 ? message = err.message : message = 'Oh no! There was a crash. Maybe try a different route?'
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
