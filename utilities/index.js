9cd4f8694749a013db01d1326b1e77879f7d5e2c
eworsham <eworsham64@gmail.com>
2024-10-02 22:16:29 -0400

    Fixed bugs and styled some css

--- utilities/index.js
+++ utilities/index.js
@@ -25,13 +25,13 @@ Util.getNav = async function (req, res, next) {
  * Build the classification view HTML
  * *************************************** */
 Util.buildClassificationGrid = async function(data) {
-    let grid
+    let grid = ''
     if (data.length > 0) {
         grid += '<ul id="inv-display">'
         data.forEach(vehicle => {
             grid += `
                 <li>
-                    <a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details>
+                    <a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">
                         <img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors">
                     </a>
                     <div class="namePrice">
